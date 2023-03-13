a49a4450e34696199bcb961bc7a7d7c5a47a1885
Aleksandr <aleksvarlaam@gmail.com>
2023-03-13 16:04:17 +0300

    Added main controller and I18n

--- config/application.rb
+++ config/application.rb
@@ -1,37 +1,46 @@
-require_relative "boot"
+# frozen_string_literal: true
 
-require "rails"
+require_relative 'boot'
+
+require 'rails'
 # Pick the frameworks you want:
-require "active_model/railtie"
-require "active_job/railtie"
-require "active_record/railtie"
-require "active_storage/engine"
-require "action_controller/railtie"
-require "action_mailer/railtie"
-require "action_mailbox/engine"
-require "action_text/engine"
-require "action_view/railtie"
-require "action_cable/engine"
+require 'active_model/railtie'
+require 'active_job/railtie'
+require 'active_record/railtie'
+require 'active_storage/engine'
+require 'action_controller/railtie'
+require 'action_mailer/railtie'
+require 'action_mailbox/engine'
+require 'action_text/engine'
+require 'action_view/railtie'
+require 'action_cable/engine'
 # require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module PhaybinPortfolio
+module ChickChuck
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
+    config.exceptions_app = routes
 
-    # Configuration for the application, engines, and railties goes here.
-    #
-    # These settings can be overridden in specific environments using the files
-    # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
+    # ActiveJob adapter
+    # config.active_job.queue_adapter = :sidekiq
+
+    # TimeZone
+    config.time_zone = 'Asia/Jerusalem'
+
+    # I18n
+    config.i18n.available_locales = %i[en he ru uk]
+    config.i18n.default_locale = :en
     # config.eager_load_paths << Rails.root.join("extras")
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+
+    # Active storage
+    config.active_storage.replace_on_assign_to_many = false
   end
 end
