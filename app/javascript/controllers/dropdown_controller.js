93d2d01741c49482682c82c16808250e81a74ebc
Aleksandr <aleksvarlaam@gmail.com>
2023-03-16 03:49:50 +0300

    Added profile layout demo

---
+++ app/javascript/controllers/dropdown_controller.js
@@ -0,0 +1,16 @@
+import { Controller } from "@hotwired/stimulus"
+import Dropdown from 'stimulus-dropdown'
+
+export default class extends Dropdown {
+  connect() {
+    super.connect()
+  }
+
+  toggle(event) {
+    super.toggle()
+  }
+
+  hide(event) {
+    super.hide(event)
+  }
+}
