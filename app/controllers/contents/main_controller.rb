63ef405bbe57428ce1b092b3da776ac54abb9eab
Aleksandr <aleksvarlaam@gmail.com>
2023-03-20 16:27:51 +0300

    Updated navigation partials and added social links demo

--- app/controllers/contents/main_controller.rb
+++ app/controllers/contents/main_controller.rb
@@ -3,7 +3,7 @@
 module Contents
   class MainController < ApplicationController
     def index
-      # @sorted_images = ActiveStorage::Attachment.where(record_type: "Category").order(created_at: :desc)
+      @sorted_images = ActiveStorage::Attachment.where(record_type: "Category").order(created_at: :desc)
     end
   end
 end
