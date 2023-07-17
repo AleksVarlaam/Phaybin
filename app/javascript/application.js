825c2f1cb15c93557f97611f1a37be66be577263
Aleksandr <aleksvarlaam@gmail.com>
2023-07-17 13:59:39 +0500

    Added stimulus scroll to controller

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,16 +1,13 @@
 import { Application } from "@hotwired/stimulus"
+import { Turbo } from "@hotwired/turbo-rails"
+import "controllers"
+import 'flowbite';
 
 const application = Application.start()
-
-// Configure Stimulus development experience
 application.debug = false
-window.Stimulus   = application
-
-export { application }
-import "controllers"
-
-// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import { Turbo } from "@hotwired/turbo-rails"
 Turbo.session.drive = true
+window.Stimulus   = application
+export { application }
 
-import 'flowbite';
+
+
